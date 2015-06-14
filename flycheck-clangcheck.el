bc8fcd73e5157d70458817656aac27ca9209519a
kumar8600 <kumar8600@gmail.com>
2015-06-14 11:37:02 +0900

    Re-license to GPLv3 and revise some texts

--- flycheck-clangcheck.el
+++ flycheck-clangcheck.el
@@ -1,37 +1,24 @@
-;;; flycheck-clangcheck.el --- Flycheck checker difinition for ClangCheck.
+;;; flycheck-clangcheck.el --- A Flycheck checker difinition for ClangCheck.
 
 ;; Author: kumar8600 <kumar8600@gmail.com>
 ;; URL: https://github.com/kumar8600/flycheck-clangcheck
-;; Version: 0.11
+;; Version: 0.20
 ;; Package-Requires: ((cl-lib "0.5") (json "1.3") (flycheck "0.17"))
 		   
-;; Copyright (c) 2014 by kumar8600
-;; All rights reserved.
+;; Copyright (c) 2015 by kumar8600 <kumar8600@gmail.com>
 
-;; Redistribution and use in source and binary forms, with or without modification,
-;; are permitted provided that the following conditions are met:
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; * Redistributions of source code must retain the above copyright notice, this
-;;   list of conditions and the following disclaimer.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-;; * Redistributions in binary form must reproduce the above copyright notice, this
-;;   list of conditions and the following disclaimer in the documentation and/or
-;;   other materials provided with the distribution.
-
-;; * Neither the name of the kumar8600 nor the names of its
-;;   contributors may be used to endorse or promote products derived from
-;;   this software without specific prior written permission.
-
-;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-;; ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-;; ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-;; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-;; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-;; ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -79,7 +66,7 @@ When non-nil, enable Considering warning as error to ClangCheck via
   "Build directory for ClangCheck.
 
 The value of this variable is a string, describing
-build directory `compile_commands.json' exists to ClangCheck via `-p'."
+a build directory where `compile_commands.json' exists."
   :type '(directory :tag "Build directory")
   :safe #'stringp)
 
